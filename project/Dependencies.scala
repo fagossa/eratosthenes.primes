0ceb99a2051135db51f0e591acf0eefbe827c860
fgu <fabian.gutierrez@fabernovel.com>
2017-12-20 09:23:39 +0100

    used readme file to redirect to the correct branch

--- project/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val akkaVersion = "2.5.3"
-  lazy val Akka = "com.typesafe.akka" %% "akka-actor" % akkaVersion
-  lazy val AkkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
-  lazy val ScalaTest = "org.scalatest" %% "scalatest" % "3.0.1" % "test"
-}
